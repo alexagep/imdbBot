7d0a78959ef203924a40c0e168c2be65e06321c4
ali.aghapour <alagha911@gmail.com>
2023-04-10 00:05:09 +0330

    define schema and using postgres and sequelize for chaching data

---
+++ model.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define('User', {
+  // Define the attributes of the model
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
