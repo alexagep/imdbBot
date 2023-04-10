f33aa54175cf480efb9c855355837a911bc3ce11
ali <alagha911@gmail.com>
2023-04-10 14:39:26 +0330

    modifying connection

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,8 @@
-const { Sequelize, Model, DataTypes } = require("sequelize");
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
 
 const sequelize = new Sequelize(
   process.env.PG_DATABASE,
